788b193c4aeb47bc3a863e462fe6aea04e91b8c6
LansananS <123167842+LansananS@users.noreply.github.com>
2025-06-25 15:28:51 +0100

    Created Events and DataHandler Classes

---
+++ events.py
@@ -0,0 +1,37 @@
+import enum as Enum
+
+#Define all events types our engine has to handle
+class EventType(Enum):
+    Market="MARKET" # New Market data
+    Signal="SIGNAL" # Our Strategy wants to enter or exit trade
+    ORDER  = "ORDER" # ORDER is executed in market
+    FILL   = "FILL" # Actual trade execution with all microstructure noises (fees, taxes) added to update portfolio
+
+
+# Base Event class, carries only the event type.
+class Event:                  
+    def __init__(self, type):
+        self.type= type         # ex: Create an Event instance: evt = Event(EventType.MARKET) then we can call evt.type
+
+# Event emitted when new market row price data is ready.
+class MarketEvent(Event):
+    def __init__(self, timestamp, symbol, price):
+        super().__init__(EventType.Market) #tag as market event
+        self.timestamp= timestamp
+        self.symbol= symbol
+        self.price= price
+
+# Event emitted by the strategy indicating a buy/sell/exit.
+class SignalEvent(Event):
+    def __init__(self, symbol, datetime, signal_type, weight):
+        super().__init__(EventType.Signal) #tag as signal event
+        self.symbol= symbol                #symbol the signal is for
+        self.datetime= datetime            #Time of signal generation
+        self.signal_type= signal_type      #Long, short or exit
+        self.weight= weight                #sizing of signal
+
+
+        
+
+
+
